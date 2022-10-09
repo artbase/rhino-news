5fadedfdc3398d9c7cf7e14e7e70985afa231925
Remix Workspace <>
2022-10-09 09:27:05 +0200

    6-Add example payable function

---
+++ contracts/6-PayableFunction.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Storage {
+
+    uint256 number;
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+
+    function deposit() public payable {}
+
+    function howmuchmoneyIsDeposited() public view returns (uint256) {
+        uint amount = address(this).balance;
+        return amount;
+    }
+
+    receive() external payable {  }
+}
