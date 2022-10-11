0c5a45ae73ab39e73820447d4bcd28bc3200bc35
Remix Workspace <>
2022-10-11 21:02:02 +0200

    5-Add example smart contract with self destructor and attack

--- contracts/5-SelfDestructorWithAttack.sol
+++ contracts/5-SelfDestructorWithAttack.sol
@@ -24,6 +24,30 @@ contract EtherGame {
     }
 }
 
+contract PreventedEtherGame {
+    uint public targetAmount = 4 ether;
+    uint public balance;
+    address public winner;
+
+    function deposit() public payable {
+        require(msg.value == 1 ether, "You can only send 1 Ether");
+
+        balance += msg.value;
+        require(balance <= targetAmount, "Game is over");
+
+        if (balance == targetAmount) {
+            winner = msg.sender;
+        }
+    }
+
+    function claimReward() public {
+        require(msg.sender == winner, "Not winner");
+
+        (bool sent, ) = msg.sender.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+    }
+}
+
 contract Attack {
     EtherGame etherGame;
 
